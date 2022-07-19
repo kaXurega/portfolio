9847aed629afd340d96a9bf09171a7f71fe6a2fb
kaXurega <edamame.dev@gmail.com>
2022-07-19 20:15:46 +0900

    Add index.tsx testing
    Performed testing and snapshot testing of the default index page header in next.js. Since the contents of index.tsx are scheduled to be changed, testing was performed to review the configuration files, etc.

---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import Home from 'pages/index';
+
+describe('snapshot', () => {
+  it('renders homepage unchanged', () => {
+    const { container } = render(<Home />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
